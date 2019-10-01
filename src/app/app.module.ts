184da6e3c5e56e40980c57795553b40359776fd0
arun.yegi94@gmail.com <Arun.Yegi@fiserv.co.in>
2019-10-01 12:13:25 +0530

    Add: Heroes module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HerosComponent } from './heros/heros.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HerosComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
